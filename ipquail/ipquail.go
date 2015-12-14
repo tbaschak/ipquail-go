3164df0bad29877a465b864ea2b883aa4535b1bf
Theodore Baschak <theodore@ciscodude.net>
2015-12-13 22:32:09 -0600

    Revert "remove binary"
    This reverts commit d8063d57433b062eea25241753c0144f8bbd98ec.

---
+++ ipquail/ipquail.go
@@ -0,0 +1,40 @@
+//  Description = ipquail website
+//  Author = Theodore Baschak
+//  Version = 1.0
+
+package main
+
+import (
+  "github.com/pilu/traffic"
+)
+
+func indexHandler(w traffic.ResponseWriter, r *traffic.Request) {
+  w.Render("ipquail")
+}
+
+func ipHandler(w traffic.ResponseWriter, r *traffic.Request) {
+  traffic.Logger().Print( r.Header.Get("X-Forwarded-For") ) 
+  w.Header().Add("Access-Control-Allow-Origin", "*")
+  w.Header().Add("Access-Control-Allow-Methods", "GET")
+  w.Header().Add("Access-Control-Allow-Headers", "X-Requested-With,Accept,Content-Type,Origin")
+  w.Header().Add("Content-type", "application/json")
+  w.WriteText( "{ \"ip\": \"" )
+  w.WriteText( r.Header.Get("X-Forwarded-For") )
+  w.WriteText( "\" }" )
+}
+
+func ptrHandler(w traffic.ResponseWriter, r *traffic.Request) {
+  w.Render("ptr")
+}
+
+func main() {
+  router := traffic.New()
+
+  // add a route for each page you add to the site
+  // make sure you create a route handler for it
+
+  router.Get("/", indexHandler)
+  router.Get("/api/ip", ipHandler)
+  router.Get("/api/ptr", ptrHandler)
+  router.Run()
+}
